ec972aea7f4df8df3fd9a5aee1c8cd883c04cbb3
Lucas Calçada <lucasmgmcalcada@gmail.com>
2025-05-15 22:11:27 -0300

    feat: add initial assembler POC

---
+++ assembler.py
@@ -0,0 +1,47 @@
+BIT_LEN = 3
+
+commands = {"HLT": 0, "STO": 1, "LD": 2}
+
+codeInstructions = []
+compiled = []
+
+with open("./code.txt", "r") as f:
+    lineIndex = 0
+    tags = {}
+    codeInstructions = f.read().split("\n")
+
+for line in codeInstructions:
+    print(f"Parsing line: {line}")
+    if line == "":
+        print("Skipping empty line")
+        continue
+
+    if line.startswith("//"):
+        print(f"Skipping comment: {line[2:]}")
+        continue
+
+    if line.endswith(":"):
+        tags[line[:-1]] = lineIndex
+
+    command = line.split(" ")[0]
+
+    if command == "HLT":
+        compiled.append(f"{lineIndex} : 0000")
+        lineIndex += 1
+        continue
+
+    param = line.split(" ")[1]
+    if command not in commands.keys():
+        raise SyntaxError(f"Command '{command}' not found")
+
+    if param in tags.keys():
+        param = tags[param]
+
+    if len(param) > BIT_LEN:
+        raise SyntaxError("Wrong word size")
+
+    param = int(param)
+    compiled.append(f"{lineIndex} : {commands[command]}{param:03}")
+    lineIndex += 1
+
+print("\n".join(compiled))
