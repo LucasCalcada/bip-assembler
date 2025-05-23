0260beea87c2b81a1247075d98dca9863f5947bd
LucasCalcada <lucasmgmcalcada@gmail.com>
2025-05-23 14:12:34 -0300

    feat: add initial code parsing

--- main.py
+++ main.py
@@ -1,4 +1,41 @@
+import argparse
+import os
+from typing import List
+
 from bip import Bip
 
+parser = argparse.ArgumentParser()
+parser.add_argument("code", type=str, help="Code file path")
+
+args = parser.parse_args()
 
 processor = Bip()
+
+
+if not os.path.exists(args.code):
+    raise FileNotFoundError("Code file not found")
+
+code: List[str] = []
+with open(args.code, "r") as f:
+    codeText = f.read()
+    code = codeText.split("\n")
+
+# Initial parse
+code.remove("")
+
+lineIndex = 0
+tags: dict[str, int] = {}
+
+for line in code:
+    # Remove comments
+    if line.startswith("#"):
+        code.pop(lineIndex)
+        continue
+
+    # Register tags addresses
+    if line.endswith(":"):
+        tagName = line[:-1]
+        tags[tagName] = lineIndex
+        continue
+
+    lineIndex += 1
