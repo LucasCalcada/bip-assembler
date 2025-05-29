825d20c33175cc2f0af65f029b0782943bf5e5f7
LucasCalcada <lucasmgmcalcada@gmail.com>
2025-05-29 10:48:43 -0300

    feat: add instruction param parsing

--- instruction.py
+++ instruction.py
@@ -1,6 +1,10 @@
 from dataclasses import dataclass
 
+from tagmanager import TagManager
 from utils import intToHex
+
+
+tags = TagManager.instance()
 
 
 @dataclass
@@ -17,5 +21,18 @@ class Instruction:
         self.name = commandName
         self.address = intToHex(address)
 
+    @staticmethod
+    def parse_param(param: str) -> str:
+        if param.startswith("0b"):
+            param = param[2:]
+            return intToHex(int(param, 2))
+        elif param.startswith("0x"):
+            param = param[2:]
+            return intToHex(int(param, 16))
+        elif param.isdigit():
+            return intToHex(int(param))
+        else:
+            return intToHex(tags.getTag(param))
+
     def parseInstruction(self, param):
-        return f"{self.address}{param.zfill(3)}"
+        return f"{self.address} : {param.zfill(3)}"
